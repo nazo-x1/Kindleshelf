260e45b0f48630f6dab26270d7faed2108bf88b6
nazo <44923159+nazo-x1@users.noreply.github.com>
2022-07-10 03:36:20 +0800

    add tls v1.x support

--- app/src/main/java/com/kunfei/bookshelf/base/BaseModelImpl.java
+++ app/src/main/java/com/kunfei/bookshelf/base/BaseModelImpl.java
@@ -1,6 +1,7 @@
 package com.kunfei.bookshelf.base;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.text.TextUtils;
@@ -19,17 +20,24 @@ import com.kunfei.bookshelf.model.impl.IHttpPostApi;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import okhttp3.Request;
+import okhttp3.TlsVersion;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import com.kunfei.bookshelf.help.Tls12SocketFactory;
+import javax.net.ssl.SSLContext;
 
 public class BaseModelImpl {
     private static OkHttpClient.Builder clientBuilder;
@@ -80,6 +88,31 @@ public class BaseModelImpl {
                 .build();
     }
 
+    public static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                return client;
+            }
+        }
+
+        return client;
+    }
+
     private static OkHttpClient.Builder getClientBuilder() {
         if (clientBuilder == null) {
             clientBuilder = new OkHttpClient.Builder()
@@ -92,7 +125,7 @@ public class BaseModelImpl {
                     .protocols(Collections.singletonList(Protocol.HTTP_1_1))
                     .addInterceptor(getHeaderInterceptor());
         }
-        return clientBuilder;
+        return enableTls12OnPreLollipop(clientBuilder);
     }
 
     private static Interceptor getHeaderInterceptor() {
